976d5053b8846e699e483fa754b6d581ea5b8abd
Graham Clapham <grahamclapham@Grahams-new-MacBook-Pro.local>
2016-09-25 08:20:59 +0100

    updated karma.config .

--- karma.conf.js
+++ karma.conf.js
@@ -34,23 +34,6 @@ module.exports = function(config) {
       'tests/*-test.js': [ 'webpack' ]
     },
 
-    typescriptPreprocessor: {
-      // options passed to the typescript compiler 
-      options: {
-        sourceMap: false, // (optional) Generates corresponding .map file. 
-        target: 'es5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5' 
-        module: 'commonjs', // (optional) Specify module code generation: 'commonjs' or 'amd' 
-        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type. 
-        noResolve: true, // (optional) Skip resolution and preprocessing. 
-        removeComments: true, // (optional) Do not emit comments to output. 
-        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false. 
-      },
-      // transforming the filenames 
-      transformPath: function(path) {
-        return path.replace(/\.ts$/, '.js');
-      }
-    },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
